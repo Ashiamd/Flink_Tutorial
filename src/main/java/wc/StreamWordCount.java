c363d60ed3dfddd8bd6719a71c1c6d76fa14e002
Ashiamd <ashiamd@foxmail.com>
2021-01-29 23:49:02 +0800

    修改StreamWordCount的注释

--- src/main/java/wc/StreamWordCount.java
+++ src/main/java/wc/StreamWordCount.java
@@ -12,18 +12,25 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class StreamWordCount {
 
     public static void main(String[] args) throws Exception {
+
+        // 创建流处理执行环境
         StreamExecutionEnvironment env = StreamContextEnvironment.getExecutionEnvironment();
+
+        // 设置并行度，默认值 = 当前计算机的CPU逻辑核数（设置成1即单线程处理）
+        // env.setMaxParallelism(32);
 
         // 从文件中读取数据
         String inputPath = "/Users/ashiamd/mydocs/docs/study/javadocument/javadocument/IDEA_project/Flink_Tutorial/src/main/resources/hello.txt";
         DataStream<String> inputDataStream = env.readTextFile(inputPath);
 
+        // 基于数据流进行转换计算
         DataStream<Tuple2<String,Integer>> resultStream = inputDataStream.flatMap(new WordCount.MyFlatMapper())
                 .keyBy(item->item.f0)
                 .sum(1);
 
         resultStream.print();
 
+        // 执行任务
         env.execute();
     }
 }
