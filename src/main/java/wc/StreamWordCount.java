471cde9eeae5851c8ff31c3f10b4989e3cf98908
Ashiamd <ashiamd@foxmail.com>
2021-01-30 00:24:49 +0800

    用parameter tool工具从程序启动参数中提取配置项

--- src/main/java/wc/StreamWordCount.java
+++ src/main/java/wc/StreamWordCount.java
@@ -1,6 +1,7 @@
 package wc;
 
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.client.program.StreamContextEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -20,8 +21,17 @@ public class StreamWordCount {
         // env.setMaxParallelism(32);
 
         // 从文件中读取数据
-        String inputPath = "/Users/ashiamd/mydocs/docs/study/javadocument/javadocument/IDEA_project/Flink_Tutorial/src/main/resources/hello.txt";
-        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+//        String inputPath = "/Users/ashiamd/mydocs/docs/study/javadocument/javadocument/IDEA_project/Flink_Tutorial/src/main/resources/hello.txt";
+//        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+
+        // 用parameter tool工具从程序启动参数中提取配置项
+        // 启动项添加参数 --host localhost --port 7777
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
+
+        // 从socket文本流读取数据
+        DataStream<String> inputDataStream = env.socketTextStream(host, port);
 
         // 基于数据流进行转换计算
         DataStream<Tuple2<String,Integer>> resultStream = inputDataStream.flatMap(new WordCount.MyFlatMapper())
