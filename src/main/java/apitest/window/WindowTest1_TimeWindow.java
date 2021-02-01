40b6994b7eabcbc6a22fbbf56133f2aa4f639bbd
Ashiamd <ashiamd@foxmail.com>
2021-02-01 23:30:00 +0800

    计数窗口，测试代码

---
+++ src/main/java/apitest/window/WindowTest1_TimeWindow.java
@@ -0,0 +1,98 @@
+package apitest.window;
+
+import apitest.beans.SensorReading;
+import org.apache.commons.collections.IteratorUtils;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+/**
+ * @author : Ashiamd email: ashiamd@foxmail.com
+ * @date : 2021/2/1 7:14 PM
+ */
+public class WindowTest1_TimeWindow {
+    public static void main(String[] args) throws Exception {
+
+        // 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 并行度设置1，方便看结果
+        env.setParallelism(1);
+
+//        // 从文件读取数据
+//        DataStream<String> dataStream = env.readTextFile("/tmp/Flink_Tutorial/src/main/resources/sensor.txt");
+
+        // 从socket文本流获取数据
+        DataStream<String> inputStream = env.socketTextStream("localhost", 7777);
+
+        // 转换成SensorReading类型
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        // 开窗测试
+
+//        // 1. 增量聚合函数 (这里简单统计每个key组里传感器信息的总数)
+//        DataStream<Integer> resultStream = dataStream.keyBy(SensorReading::getId)
+////                .countWindow(10, 2);
+////                .window(EventTimeSessionWindows.withGap(Time.minutes(1)));
+////                .window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
+////                .timeWindow(Time.seconds(15)) // 已经不建议使用@Deprecated
+//                .window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
+//                .aggregate(new AggregateFunction<SensorReading, Integer, Integer>() {
+//
+//                    // 新建的累加器
+//                    @Override
+//                    public Integer createAccumulator() {
+//                        return 0;
+//                    }
+//
+//                    // 每个数据在上次的基础上累加
+//                    @Override
+//                    public Integer add(SensorReading value, Integer accumulator) {
+//                        return accumulator + 1;
+//                    }
+//
+//                    // 返回结果值
+//                    @Override
+//                    public Integer getResult(Integer accumulator) {
+//                        return accumulator;
+//                    }
+//
+//                    // 分区合并结果(TimeWindow一般用不到，SessionWindow可能需要考虑合并)
+//                    @Override
+//                    public Integer merge(Integer a, Integer b) {
+//                        return a + b;
+//                    }
+//                });
+//
+//        resultStream.print("result");
+
+
+        // 2. 全窗口函数 （WindowFunction和ProcessWindowFunction，后者更全面）
+        SingleOutputStreamOperator<Tuple3<String, Long, Integer>> resultStream2 = dataStream.keyBy(SensorReading::getId)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
+//                .process(new ProcessWindowFunction<SensorReading, Object, Tuple, TimeWindow>() {
+//                })
+                .apply(new WindowFunction<SensorReading, Tuple3<String, Long, Integer>, String, TimeWindow>() {
+                    @Override
+                    public void apply(String s, TimeWindow window, Iterable<SensorReading> input, Collector<Tuple3<String, Long, Integer>> out) throws Exception {
+                        String id = s;
+                        long windowEnd = window.getEnd();
+                        int count = IteratorUtils.toList(input.iterator()).size();
+                        out.collect(new Tuple3<>(id, windowEnd, count));
+                    }
+                });
+
+        resultStream2.print("result2");
+
+        env.execute();
+    }
+}
---
+++ src/main/java/apitest/window/WindowTest2_CountWindow.java
@@ -0,0 +1,63 @@
+package apitest.window;
+
+import apitest.beans.SensorReading;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author : Ashiamd email: ashiamd@foxmail.com
+ * @date : 2021/2/1 11:03 PM
+ */
+public class WindowTest2_CountWindow {
+    public static void main(String[] args) throws Exception {
+
+        // 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 并行度设置1，方便看结果
+        env.setParallelism(1);
+
+        // 从socket文本流获取数据
+        DataStream<String> inputStream = env.socketTextStream("localhost", 7777);
+
+        // 转换成SensorReading类型
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        DataStream<Double> resultStream = dataStream.keyBy(SensorReading::getId)
+                .countWindow(10, 2)
+                .aggregate(new MyAvgFunc());
+
+        resultStream.print("result");
+
+        env.execute();
+    }
+
+    public static class MyAvgFunc implements AggregateFunction<SensorReading, Tuple2<Double, Integer>, Double> {
+
+        @Override
+        public Tuple2<Double, Integer> createAccumulator() {
+            return new Tuple2<>(0.0, 0);
+        }
+
+        @Override
+        public Tuple2<Double, Integer> add(SensorReading value, Tuple2<Double, Integer> accumulator) {
+            // 温度累加求和，当前统计的温度个数+1
+            return new Tuple2<>(accumulator.f0 + value.getTemperature(), accumulator.f1 + 1);
+        }
+
+        @Override
+        public Double getResult(Tuple2<Double, Integer> accumulator) {
+            return accumulator.f0 / accumulator.f1;
+        }
+
+        @Override
+        public Tuple2<Double, Integer> merge(Tuple2<Double, Integer> a, Tuple2<Double, Integer> b) {
+            return new Tuple2<>(a.f0 + b.f0, a.f1 + b.f1);
+        }
+    }
+}
