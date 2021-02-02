c187a6947e19e78330378a8dc56fa428ce1d524f
Ashiamd <ashiamd@foxmail.com>
2021-02-03 04:22:20 +0800

    修改了 容错机制测试代码的注释

--- src/main/java/apitest/state/StateTest4_FaultTolerance.java
+++ src/main/java/apitest/state/StateTest4_FaultTolerance.java
@@ -25,21 +25,26 @@ public class StateTest4_FaultTolerance {
         // 这个需要另外导入依赖
         env.setStateBackend(new RocksDBStateBackend(""));
 
-        // 2. 检查点配置
+        // 2. 检查点配置 (每300ms让jobManager进行一次checkpoint检查)
         env.enableCheckpointing(300);
 
         // 高级选项
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        //Checkpoint的处理超时时间
         env.getCheckpointConfig().setCheckpointTimeout(60000L);
+        // 最大允许同时处理几个Checkpoint(比如上一个处理到一半，这里又收到一个待处理的Checkpoint事件)
         env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
+        // 与上面setMaxConcurrentCheckpoints(2) 冲突，这个时间间隔是 当前checkpoint的处理完成时间与接收最新一个checkpoint之间的时间间隔
         env.getCheckpointConfig().setMinPauseBetweenCheckpoints(100L);
+        // 如果同时开启了savepoint且有更新的备份，是否倾向于使用更老的自动备份checkpoint来恢复，默认false
         env.getCheckpointConfig().setPreferCheckpointForRecovery(true);
+        // 最多能容忍几次checkpoint处理失败（默认0，即checkpoint处理失败，就当作程序执行异常）
         env.getCheckpointConfig().setTolerableCheckpointFailureNumber(0);
 
         // 3. 重启策略配置
-        // 固定延迟重启
+        // 固定延迟重启(最多尝试3次，每次间隔10s)
         env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 10000L));
-        // 失败率重启
+        // 失败率重启(在10分钟内最多尝试3次，每次至少间隔1分钟)
         env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.minutes(10), Time.minutes(1)));
 
         // socket文本流
