63777bc0cf3512bbd735486587396f3150ae1104
Ashiamd <ashiamd@foxmail.com>
2021-02-03 05:51:43 +0800

    Table API初体验，代码测试

---
+++ src/main/java/apitest/tableapi/TableTest1_Example.java
@@ -0,0 +1,48 @@
+package apitest.tableapi;
+
+import apitest.beans.SensorReading;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+/**
+ * @author : Ashiamd email: ashiamd@foxmail.com
+ * @date : 2021/2/3 5:47 AM
+ */
+public class TableTest1_Example {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 1. 读取数据
+        DataStream<String> inputStream = env.readTextFile("/Users/ashiamd/mydocs/docs/study/javadocument/javadocument/IDEA_project/Flink_Tutorial/src/main/resources/sensor.txt");
+
+        // 2. 转换成POJO
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        // 3. 创建表环境
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        // 4. 基于流创建一张表
+        Table dataTable = tableEnv.fromDataStream(dataStream);
+
+        // 5. 调用table API进行转换操作
+        Table resultTable = dataTable.select("id, temperature")
+                .where("id = 'sensor_1'");
+
+        // 6. 执行SQL
+        tableEnv.createTemporaryView("sensor", dataTable);
+        String sql = "select id, temperature from sensor where id = 'sensor_1'";
+        Table resultSqlTable = tableEnv.sqlQuery(sql);
+
+        tableEnv.toAppendStream(resultTable, Row.class).print("result");
+        tableEnv.toAppendStream(resultSqlTable, Row.class).print("sql");
+
+        env.execute();
+    }
+}
